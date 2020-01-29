75e7b9a0b42605b9b88df143269f8f2762b48011
aphrx <amalnnath20@gmail.com>
2020-01-29 12:02:23 -0500

    Initial commit

---
+++ BluetoothListener.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+public class BluetoothListener {
+	public static void main(String args[]){
+
+		String s = null;
+		int conn = 0;
+
+		try {
+			Process p = Runtime.getRuntime().exec("hcitool con");	
+			BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+			while((s = stdInput.readLine()) != null){
+				if(s.contains("00:15:83:00:43:EC")){
+					System.out.println("Connection to headunit was successful!");
+					conn++;
+				}
+			}
+			if(conn == 0){
+				System.out.println("Connection to headunit was not successful!");
+			}
+		} 
+		catch (IOException e) {
+			System.out.println(e);
+		}	
+	}
+}
